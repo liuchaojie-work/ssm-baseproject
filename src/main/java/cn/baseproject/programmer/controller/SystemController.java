cefb0b75d2f08a8edfce63af62844e376c893a69
liuchaojie-work <liuchaojie_work@163.com>
2020-05-27 12:22:58 +0800

    【修改错误】jstl依赖不起作用，必须放入tomcat中lib文件夹

--- src/main/java/cn/baseproject/programmer/controller/SystemController.java
+++ src/main/java/cn/baseproject/programmer/controller/SystemController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SystemController {
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(){
-        return "index";
+        return "system/index";
     }
 }
